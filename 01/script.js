74baa8c2b98d28eb634f9278b9a562b8bc9b6143
Minhaj <minhajazeem91@gmail.com>
2023-04-07 06:06:46 +0500

    1st Assignment 01 Quiz

---
+++ 01/script.js
@@ -0,0 +1,7 @@
+const a = prompt("Please Enter First Number: ");
+const b = prompt("Please Enter Second Number: ");
+
+console.log("First Number: ", a);
+console.log("Second Number: ", b);
+
+console.log("Larger Number is: ", Math.max(a, b));
