5b1270c01ba8f9690cf9f920ad845fbefc0f251b
Minhaj <minhajazeem91@gmail.com>
2023-04-07 15:37:20 +0500

    Alert Updated

--- 02/script.js
+++ 02/script.js
@@ -1,7 +1,7 @@
 const InputVal = prompt("Enter a number");
 console.log("Entered Number: " , InputVal);
 if(InputVal >= 0) {
-    console.log("The sign is +");
+    alert("The sign is +");
 } else{
-    console.log("The sign is -");
+    alert("The sign is -");
 }
---
+++ 03/script.js
@@ -0,0 +1,13 @@
+const firstNum = prompt("Enter First Number");
+const secNum = prompt("Enter Second Number");
+const thirdNum = prompt("Enter Third Number");
+const fourthNum = prompt("Enter Fourth Number");
+const fifthNum = prompt("Enter Fifth Number");
+
+console.log("First Number :", firstNum);
+console.log("Second Number :", secNum);
+console.log("Third Number :", thirdNum);
+console.log("Fourth Number :", fourthNum);
+console.log("Fifth Number :", fifthNum);
+
+
